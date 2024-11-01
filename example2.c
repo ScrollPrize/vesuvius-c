93fba535481b8a01b48cd3ae5deaa1abb7f27509
SuperOpt <156155735+SuperOptimizer@users.noreply.github.com>
2024-11-01 13:59:08 -0500

    Create example2.c

---
+++ example2.c
@@ -0,0 +1,13 @@
+#include "volcano.h"
+
+int main(int argc, char** argv) {
+  //Scroll 1 20230205180739
+  volume* vol = volume_new((s32[3]){14376,7888,8096}, false, true, true, "./cache", 20230205180739);
+
+  s32 start[3] = {2000,2000,2000};
+  s32 dims[3] = {500,500,500};
+
+
+  chunk* mychunk = volume_get_chunk(vol,start,dims);
+  return 0;
+}
